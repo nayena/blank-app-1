1f55788d21b4c4ed8f001597306afe49bd5d38fd
ashi-kamra <ak118@wellesley.edu>
2024-11-09 14:51:54 -0500

    adding title and subtitle

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,12 @@ import google.generativeai as genai
 genai.configure(api_key="AIzaSyAB7IqPaUC3lNWFL5YfKpb2zlAVizz06ag")
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-response = model.generate_content("How many planets are in our solar system?")
+
+
+st.title("Financial Literacy App Title")
+st.subheader("Introduction Blurb.")
+
+# response = model.generate_content("How many planets are in our solar system?")
 
 st.write("Testing Gemini Output")
 st.write(response.text)
