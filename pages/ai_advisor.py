0feb80dfd59ed4b2ed2679350831fdf81c2486a5
natasha <mnatasha737@gmail.com>
2024-12-09 11:51:24 -0500

    added model

--- pages/ai_advisor.py
+++ pages/ai_advisor.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 load_dotenv()
 genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
 
+model = genai.GenerativeModel("gemini-1.5-flash")
+
 st.title("Cash Course AI Advisor 🦾")
 st.caption("🚀 Powered by Google Gemini")
 
