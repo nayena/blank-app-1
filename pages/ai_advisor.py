1c53354017c679e8b8ddab7251d7fe4cacc2f0bd
natasha <mnatasha737@gmail.com>
2024-12-09 11:57:14 -0500

    fixed

--- pages/ai_advisor.py
+++ pages/ai_advisor.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 
